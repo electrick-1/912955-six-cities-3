21c5a5e1068a2d3b7efb026a4cf116a870ef4ab4
electrick-1 <elbandido93@mail.ru>
2020-03-25 00:38:14 +0700

    Если нет предложений, то выводится соответствующее окно

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -16,6 +16,49 @@ class Main extends PureComponent {
       sortedOffers
     } = this.props;
 
+    const isOffers = () => {
+      if (sortedOffers.length === 0) {
+        return (
+          <div className="cities">
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{sortedOffers.length} places to stay in {currentCity}</b>
+                <SortList />
+                <PlacesList
+                  cardClass={cardClass}
+                  onTitleClick={onTitleClick}
+                  onMouseEnter={onMouseEnter}
+                />
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <Map
+                    sortedOffers={sortedOffers}
+                    activeOffer={activeOffer}
+                  />
+                </section>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    };
+
     return (
       <div className="page page--gray page--main">
         <header className="header">
@@ -41,31 +84,10 @@ class Main extends PureComponent {
           </div>
         </header>
 
-        <main className="page__main page__main--index">
+        <main className={`page__main page__main--index ${sortedOffers.length === 0 ? `page__main--index-empty` : ``}`}>
           <h1 className="visually-hidden">Cities</h1>
           <CitiesList />
-          <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{sortedOffers.length} places to stay in {currentCity}</b>
-                <SortList />
-                <PlacesList
-                  cardClass={cardClass}
-                  onTitleClick={onTitleClick}
-                  onMouseEnter={onMouseEnter}
-                />
-              </section>
-              <div className="cities__right-section">
-                <section className="cities__map map">
-                  <Map
-                    sortedOffers={sortedOffers}
-                    activeOffer={activeOffer}
-                  />
-                </section>
-              </div>
-            </div>
-          </div>
+          {isOffers()}
         </main>
       </div>
     );
